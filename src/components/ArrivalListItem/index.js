c61fa9306616bbccebe4433038607cc194fbb383
Taylor <tjohnson@expapp.com>
2019-06-23 15:06:18 -0400

    Color indicators for arrivals list

--- src/components/ArrivalListItem/index.js
+++ src/components/ArrivalListItem/index.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import styled from "styled-components";
 import { colorCodeArrivalTime, prettyTime } from "../../utils/utils";
 import {
-    brand_darkest_grey,
-    brand_lighter_grey
+    brand_blue,
+    brand_darkest_grey, brand_gold, brand_green,
+    brand_lighter_grey, brand_lightest_grey, brand_red
 } from "../../utils/colors";
 
 const Arrival = styled.div`
@@ -13,21 +14,35 @@ const Arrival = styled.div`
     align-items: center;
     border-bottom: 1px solid ${brand_lighter_grey};
     cursor: pointer;
-    padding: 0 20px 0 0;
+    padding: 0;
     position: relative;
+    background: ${({isSelected}) => isSelected ? brand_lightest_grey : null};
+    transition: background 0.25s ease;
     
 `;
 
 const Line = styled.div`
     width: 30%;
-    border-radius: 5px;
-    background-color: #FFFFFF;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-flow: column nowrap;
     font-size: 24px;
     color: ${brand_darkest_grey};
+    border-right: 15px solid;
+    border-radius: 15px;
+    border-color: ${({line}) => {
+        switch(line) {
+            case 'Blue':
+                return brand_blue;
+            case 'Red':
+                return brand_red;
+            case 'Gold':
+                return brand_gold;
+            case 'Green':
+                return brand_green;
+        }    
+    }};
 `;
 
 const ArrivalHead = styled.div`
@@ -35,8 +50,7 @@ const ArrivalHead = styled.div`
     flex-flow: row wrap;
     text-align: end;
     justify-self: flex-end;
-    padding: 30px 0;
-    border-left: 1px solid ${brand_lighter_grey};
+    padding: 30px 20px;
     flex: 1 1;
 `;
 
@@ -55,10 +69,16 @@ const Direction = styled.div`
 const Details = styled.div`
     width: 100%;
     font-size: 14px;
-    transition: max-height 0.25s ease;
+    transition: all 0.25s ease;
     max-height: ${({isSelected}) => isSelected ? '2000px' : '0'};
     overflow: hidden;
     
+    > div:first-of-type {
+        padding: 20px 15px 10px;
+    }
+    > div:last-of-type {
+        padding: 10px 15px 20px;
+    }
 `;
 
 const DetailItem = styled.div`
@@ -86,7 +106,7 @@ const ArrivalListItem = (props) => {
     };
 
     return (
-        <Arrival onClick={handleClick}>
+        <Arrival isSelected={isSelected} onClick={handleClick}>
             <Line line={line}>
                 <div>{line}</div>
             </Line>
