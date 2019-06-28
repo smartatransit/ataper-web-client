f331492952ac34814c40450e6003a66bdd047717
Taylor <tjohnson@expapp.com>
2019-06-28 17:42:21 -0400

    Darker bg for modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -9,7 +9,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     bottom: 0;
-    background: rgba(0,0,0,0.6);
+    background: rgba(0,0,0,0.8);
     display: flex;
     align-items: center;
     justify-content: center;
